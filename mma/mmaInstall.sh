16528299c0ff132156c3c00d5da72794398ea137
labmixz <labmixz@gmail.com>
2013-10-17 11:55:05 -0400

    Delete mmaInstall.sh

--- mma/mmaInstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "\n\n# cron jobs for MobileMinerApp add-on" >> /var/spool/cron/minepeon
-echo "*/1 * * * * /usr/bin/php /home/minepeon/addons/mma/mobileminerapp.cron.php update" >> /var/spool/cron/minepeon
-echo "*/2 * * * * /usr/bin/php /home/minepeon/addons/mma/mobileminerapp.cron.php check" >> /var/spool/cron/minepeon
