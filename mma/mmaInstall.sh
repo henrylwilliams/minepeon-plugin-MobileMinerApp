835c79fa5f3e75edd9744a910ba7382cd47f8ad8
labmixz <labmixz@gmail.com>
2013-10-17 10:47:55 -0400

    removed -e from first line echo

--- mma/mmaInstall.sh
+++ mma/mmaInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo -e "\n\n# cron jobs for MobileMinerApp add-on" >> /var/spool/cron/minepeon
+echo "\n\n# cron jobs for MobileMinerApp add-on" >> /var/spool/cron/minepeon
 echo "*/1 * * * * /usr/bin/php /home/minepeon/addons/mma/mobileminerapp.cron.php update" >> /var/spool/cron/minepeon
 echo "*/2 * * * * /usr/bin/php /home/minepeon/addons/mma/mobileminerapp.cron.php check" >> /var/spool/cron/minepeon
